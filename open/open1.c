a835b903da5a8e5ba46a9a6461928fabd2002bb7
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2021-09-17 23:09:33 +0530

    added new comments

--- open/open1.c
+++ open/open1.c
@@ -4,12 +4,12 @@
 #include <sys/stat.h>
 
 int main (){
-	int n, fd, fd1; // always fd > 2
+	int n, fd, fd1; 		// always fd > 2
 	char buffer[50];
 	fd = open("test.txt", O_RDONLY); // opens or possibly create a new file and return file descriptor whose value is always greter than 2 
 	                                // as 0, 1, 2 are already used for some other purpose
-	// open(const char* pathname, int flag) for creating 
-	// open(const char* pathname, int flag, int mode)
+					// use open(const char* pathname) for creating new file
+					// use open(const char* pathname, int flag, int mode) for existing file
 	n = read(fd, buffer, 10);
 	write(1, buffer, n);
 	return 0;
