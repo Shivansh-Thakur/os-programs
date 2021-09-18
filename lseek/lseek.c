6538dff8f9a02959dc94f9bc129e27218a536ff1
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2021-09-18 18:20:09 +0530

    added comments

--- lseek/lseek.c
+++ lseek/lseek.c
@@ -11,7 +11,11 @@ int main (){
 	read(fd, buffer, 10);
 	write(1, buffer, 10);
 	fd = open("seeking",O_RDWR);
-	f = lseek(fd, 10, SEEK_CUR);
+	f = lseek(fd, 10, SEEK_CUR);// used to reposition file pointer and return repositioned pointer 
+				    // lseek(int fd, off_t offset , int whenc)
+				    // where fd is file descriptor of the file you want to read/write
+				    // offset is 'where to set the pointer'
+				    // whenc is 'from where to set the pointer' using SEEK_SET, SEEK_CUR, SEEK_END
 	printf("\ncurrent position is %d\n", f);
 	n = read(fd, buffer, 10);
 	write(1, buffer, n);
