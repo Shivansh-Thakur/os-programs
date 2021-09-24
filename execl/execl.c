2ac6b4d380c1b367bb1066b81d8ec3d3f2ee9dd8
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2021-09-24 20:45:40 +0530

    Update execl.c

--- execl/execl.c
+++ execl/execl.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
 
+int main(void)
+{
+    printf("\n Current process pid = %d will be assigned to ps.\n", getpid());
+    execl("/bin/ps", "ps", NULL);// execl will makes a new image of the whole code and run that code
+                                 // here in this code it is running ps command's code
+    printf("After\n");
+
+    return 0;
+}
