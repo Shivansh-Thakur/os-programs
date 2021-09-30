105c7cf43c7b545186ff074dc7a69b6136184f46
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2021-09-30 23:04:47 +0530

    Update ipc-write.c

--- inter-process-communication/ipc-write.c
+++ inter-process-communication/ipc-write.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
+int main(void)
+{
+    FILE *fd;
+    char buffer[50];
+    sprintf(buffer, "hello world");
+    fd = popen("wc -c", "w"); // FILE *popen(const char *command, const char *type)
+    fwrite(buffer, sizeof(char), strlen(buffer), fd);
+    pclose(fd);
+}
