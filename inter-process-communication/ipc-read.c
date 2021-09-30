8d0a200847c1fd9679b95d1b6bbca2f332fde3c7
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2021-09-30 23:04:29 +0530

    Update ipc-read.c

--- inter-process-communication/ipc-read.c
+++ inter-process-communication/ipc-read.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
+int main(void)
+{
+    FILE *fd;
+    char buffer[50];
+    fd = popen("ls", "r"); // FILE *popen(const char *command, const char *type)
+    fread(buffer, sizeof(char), 12, fd);
+    write(1, buffer, 12); 
+    printf("\n");
+    pclose(fd);
+}
