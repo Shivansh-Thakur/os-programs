3fada8878a97590981a0548a557c4bfcd912bc68
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2021-10-01 22:55:37 +0530

    Update ipc-pipe.c

--- inter-process-communication/ipc-pipe.c
+++ inter-process-communication/ipc-pipe.c
@@ -8,7 +8,8 @@ int main(void)
     int fd[2], n;
     char buffer[50];
     
-    pipe(fd);
+    pipe(fd); // pipe() function call is used to create two file descriptors 
+             // first one is fd[0] for reading end and second one is fd[1] for writing end of the file.
     p = fork(); 
     if(p > 0) { // inside parent
         printf("Reading data from the parent... \n");
