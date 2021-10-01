12df832d49bc66e653e541ef71969f6dcdc46fef
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2021-10-01 22:47:40 +0530

    Update ipc-pipe.c

--- inter-process-communication/ipc-pipe.c
+++ inter-process-communication/ipc-pipe.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
+int main(void)
+{
+    pid_t p;
+    int fd[2], n;
+    char buffer[50];
+    
+    pipe(fd);
+    p = fork(); 
+    if(p > 0) { // inside parent
+        printf("Reading data from the parent... \n");
+        write(fd[1], "hello are you there??\n", 22);
+    
+    } else if(p == 0) { // inside child
+        printf("Writing to a child process...\n");
+        n = read(fd[0], buffer, 50);
+        write(1, buffer, n);
+    
+    } else {
+        printf("error");
+    }
+}
