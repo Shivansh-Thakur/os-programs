3081f8df8d96370a3cb7e16e0cd87b99ab78fa5c
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2021-10-02 23:50:53 +0530

    Update receiver-mkfifo.c

--- mkfifo/receiver-mkfifo.c
+++ mkfifo/receiver-mkfifo.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+int main(void)
+{
+    int res, n;
+    char buffer[100];
+    res = open("fifo1", O_RDONLY);
+    //res = open("fifo1", O_NONBLOCK, O_RDONLY); //for non blocked mode
+    n = read(res, buffer, 100);
+    printf("Reader processs having pid %d has started\n", getpid());
+    printf("Data received by the receiver %d is : %s\n", getpid(), buffer);
+}
