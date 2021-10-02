19f032be1e050f250d0cbefdce3756487d5b536e
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2021-10-02 23:50:38 +0530

    Update sender-mkfifo.c

--- mkfifo/sender-mkfifo.c
+++ mkfifo/sender-mkfifo.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+int main(void)
+{
+    int res;
+    res = open("fifo1", O_WRONLY);
+    //res = open("fifo1",O_NONBLOCK, O_WRONLY);  //for non blocked mode
+    write(res, "message", 7);
+    printf("Sender processs having pid %d sent the data\n", getpid());
+}
